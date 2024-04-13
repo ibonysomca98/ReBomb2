534f71eddae4ce50d5b95f0f286f76e4430139f8
ibonysomca98 <166942728+ibonysomca98@users.noreply.github.com>
2024-04-13 11:40:33 -0600

    Update ReBomb2.py

--- ReBomb2.py
+++ ReBomb2.py
@@ -27,7 +27,7 @@ print("Tutorial for this is on my Youtube: BioRat")
 print("")
 print("")
 
-x = input('Enter the request URL from Inspect Element: ')
+x = input('Enter the request URL from Inspect Element: https://www.tiktok.com/api/feedback/v1/newest_reply/?WebIdLastTime=1713024966&aid=1284&app_key=tiktok-web&app_language=en&app_name=tiktok_web&appkey=tiktok-web&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=Win32&browser_version=5.0%20%28Windows%20NT%2010.0%3B%20Win64%3B%20x64%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F123.0.0.0%20Safari%2F537.36&channel=tiktok_web&cookie_enabled=true&device_id=7357386104154949125&device_platform=web_pc&focus_state=false&from_page=user&history_len=9&iid=0&is_fullscreen=false&is_page_visible=false&os=windows&priority_region=&referer=&region=SV&screen_height=720&screen_width=1280&tz_name=America%2FEl_Salvador&verifyFp=verify_luybo3wx_fTOPhHuT_e42s_49vn_8WtB_Kb0WS1MrBDmf&we ')
 print("")
 print("")
 
